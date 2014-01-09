6678655d99d4f8f74442455a99d7064d631fe940
Torstein Saltvedt <torstein.saltvedt@gmail.com>
2014-01-09 13:18:18 +0100

    Always sign in test cases via remember token.

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,10 +1,10 @@
 include ApplicationHelper
 
 def sign_in(user)
-  visit signin_path
-  fill_in I18n.t("session.email"),    with: user.email
-  fill_in I18n.t("session.password"), with: user.password
-  click_button I18n.t("session.sign_in")
+  #visit signin_path
+  #fill_in I18n.t("session.email"),    with: user.email
+  #fill_in I18n.t("session.password"), with: user.password
+  #click_button I18n.t("session.sign_in")
   # Sign in when not using Capybara as well.
   cookies[:remember_token] = user.remember_token
 end
